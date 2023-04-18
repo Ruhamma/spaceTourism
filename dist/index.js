fe0006bbadca74e6d2c09ad76f5f4a0dbd81f904
Ruhamma Ashenafi <ruhammaashenafi202@gmail.com>
2023-04-18 17:45:05 +0300

    Add scene changing functionality to destination page

--- dist/index.js
+++ dist/index.js
@@ -18,18 +18,29 @@ const planetName = document.getElementById("planetName");
 const desc = document.getElementById("desc");
 const distance = document.getElementById("distance");
 const travel = document.getElementById("travel");
+const planetImage = document.getElementById("planet-image");
+const tabContainer = document.getElementById("tabContainer");
+
 function changeScene(data, i) {
   distance.innerHTML = data.destinations[i].distance;
   planetName.innerHTML = data.destinations[i].name;
   desc.innerHTML = data.destinations[i].description;
   travel.innerHTML = data.destinations[i].travel;
+  planetImage.src = data.destinations[i].images.png;
 }
+
 for (let i = 0; i < destinations.length; i++) {
+  
   destinations[i].addEventListener("click", () => {
+    
+    console.log(tabContainer);
+tabContainer
+  .querySelector('[aria-selected="true"]')
+  .setAttribute("aria-selected", false);
+
+    destinations[i].setAttribute("aria-selected", true);
     fetch("./data.json")
       .then((response) => response.json())
       .then((data) => changeScene(data, i));
   });
 }
-
-
